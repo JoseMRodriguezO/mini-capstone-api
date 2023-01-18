40b025aad7eaab18095a710cdff7044a35f34338
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-01-17 20:55:11 -0600

    db seedspractice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: "glasses", price: 200, description: "beautiful glasses", image_url: "")
+Product.create(name: "sunglasses", price: 42, description: "oakle sunglasses.", image_url: "")
+Product.create(name: "Air Jordan one", price: 270, description: "most awesome shoe", image_url: "")
+Product.create(name: "Adidasshoe", price: 170, description: "another great shoe ", image_url: "")
+Product.create(name: "Air Jordan one low", price: 57, description: "These are part of my collection", image_url: "")
+Product.create(name: "zion 2 Jordan shoe", price: 9, description: "", image_url: "")
+Product.create(name: "Yoda sleeping bag", price: 40, description: "For real", image_url: "https://staticdelivery.nexusmods.com/mods/1151/images/12353-0-1461721930.png")
