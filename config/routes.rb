0939db9f27fa10305304c531bef3618b983a5530
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-01-18 19:22:28 -0600

    parameters

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/all_products", controller: "application", action: "all_products"
-  get "/one_product/:id", controller: "application", action: "one_product"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
 end
