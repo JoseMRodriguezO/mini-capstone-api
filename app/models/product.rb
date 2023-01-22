b5cf8ddcda0b36f705ac4bb5a8f7e04fde1d7233
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-01-22 10:37:46 -0600

    model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price <= 10 
+      return true 
+    else 
+      return false
+  end
+
+  def tax
+    return price * 0.09
+  end
+
+  def total
+    return price + tax
+  end
 end
