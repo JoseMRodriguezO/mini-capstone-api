000e0f92b31564bf89102e38baa1d4f0d3fbad71
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-01-23 21:04:17 -0600

    added test for good/bad path

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,6 +7,9 @@ end
 test "create" do
   assert_difference "Product.count", 1 do
     post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+
+    post "/products.json", params: {}
+    assert_response 422
   end
   test "update" do
     product = Product.first
@@ -18,6 +21,9 @@ test "create" do
     assert_equal product.price, data["price"]
     assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
+
+    patch "/products/#{product.id}.json", params: { name: "" }
+    assert_response 422
   end
   test "destroy" do
     assert_difference "Product.count", -1 do
