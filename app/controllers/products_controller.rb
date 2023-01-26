0ef05068bcac275f50b87f4cb53da7d402101bcd
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-01-25 18:01:18 -0600

    fixed application controler test and product controler

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,47 @@
 class ProductsController < ApplicationController
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def create
+    @product = Product.create(
+      supplier_id: params[:supplier_id],
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    if @product.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @product = Product.find_by(id: params["id"])
+    render :show
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["image_url"] || @product.image_url,
+      description: params["description"] || @product.description,
+    )
+    if @product.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "yo." }
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,4 +16,25 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["id", "name", "price", "tax", "total", "is_discounted?", "image_url", "description", "supplier", "created_at", "updated_at"], data.keys
   end
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { supplier_id: Supplier.first.id, price: 1, name: "test product", description: "test description", image_url: "image.jpg" }
+      data = JSON.parse(response.body)
+      assert_response 200
+    end
+    post "/products.json", params: {}
+    assert_response 422
+  end
+  test "update" do
+    product = Product.first
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+    assert_equal product.price.to_s, data["price"]
+    assert_equal product.image_url, data["image_url"]
+    assert_equal product.description, data["description"]
+    patch "/products/#{product.id}.json", params: { name: "" }
+    assert_response 422
+  end
 end
