72c23386d888d113db0e8c6bb68e70da106551a5
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-01-26 19:46:58 -0600

    added user and password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_005203) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_012350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_005203) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
-
+    t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,4 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_005203) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
