113e17b499a988de34403b5a4cc8d2ba798a49d5
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-02 19:35:28 -0600

    added created action to cartedproduct and jbuilder

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,11 +1,10 @@
 class CartedProductsController < ApplicationController
   def create
     @carted_product = CartedProduct.create(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
-      order_id: params[:order_id],
+      status: "carted",
     )
     render :show
   end
